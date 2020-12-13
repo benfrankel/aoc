cec2f4aa0721cc489e5f1b9363056418283be288
Ben Frankel <ben.frankel7@gmail.com>
2020-12-12 19:28:34 -0800

    Time solutions

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(unused_imports)]
 
 use std::collections::HashMap;
+use std::time::{Duration, Instant};
 
 pub mod aoc;
 pub mod prelude;
@@ -9,17 +10,81 @@ pub mod util;
 
 pub type Solver = Box<dyn Fn(&str)>;
 
+pub fn time<T, F>(
+    max_iterations: usize,
+    max_duration: Duration,
+    f: F,
+) -> (Vec<Duration>, T)
+where
+    F: Fn() -> T,
+{
+    assert!(max_iterations >= 1);
+    
+    let mut elapsed = vec![];
+    let mut output = None;
+    let loop_start = Instant::now();
+    for _ in 0..max_iterations {
+        let start = Instant::now();
+        output = Some(f());
+        let end = Instant::now();
+        
+        elapsed.push(end - start);
+        if end - loop_start >= max_duration {
+            break;
+        }
+    }
+    
+    (elapsed, output.unwrap())
+}
+
+pub fn summarize(elapsed: &[Duration]) {
+    println!(
+        "    Min duration: {} μs",
+        elapsed.iter().min().unwrap().as_micros(),
+    );
+    println!(
+        "    Avg duration: {} μs",
+        elapsed.iter().sum::<Duration>().as_micros() as f32 / elapsed.len() as f32,
+    );
+    println!(
+        "    Max duration: {} μs",
+        elapsed.iter().max().unwrap().as_micros(),
+    );
+}
+
 #[macro_export]
 macro_rules! push_solver {
     ($solvers:ident, $prefix:ident, $day:ident) => {
-        $solvers.insert(
-            $prefix.clone() + stringify!($day),
-            Box::new(|input: &str| {
-                let parsed = $day::parse(input);
-                println!("Part 1: {}", $day::part1(&parsed));
-                println!("Part 2: {}", $day::part2(&parsed));
-            }) as crate::Solver,
-        );
+        {
+            $solvers.insert(
+                $prefix.clone() + stringify!($day),
+                Box::new(|input: &str| {
+                    let (elapsed, parsed) = crate::time(
+                        1000,
+                        std::time::Duration::from_millis(100),
+                        || $day::parse(input),
+                    );
+                    println!("Parsing:");
+                    crate::summarize(&elapsed);
+                    
+                    let (elapsed, output) = crate::time(
+                        1000,
+                        std::time::Duration::from_millis(100),
+                        || $day::part1(&parsed),
+                    );
+                    println!("Part 1: {}", output);
+                    crate::summarize(&elapsed);
+                    
+                    let (elapsed, output) = crate::time(
+                        1000,
+                        std::time::Duration::from_millis(100),
+                        || $day::part2(&parsed),
+                    );
+                    println!("Part 2: {}", output);
+                    crate::summarize(&elapsed);
+                }) as crate::Solver,
+            );
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-use std::collections::HashMap;
-
 use itertools::Itertools;
 
 fn main() {
     let path = std::env::args().skip(1).join("/");
-    solve(&solve::solvers(), path);
-}
-
-fn solve(
-    solvers: &HashMap<String, solve::Solver>,
-    key: String,
-) {
-    let input = "input/".to_string() + &key;
-    let input = match std::fs::read_to_string(&input) {
-        Ok(input) => input,
-        Err(_) => {
+    let input = "input/".to_string() + &path;
+    let input = std::fs::read_to_string(&input)
+        .unwrap_or_else(|_| {
             eprintln!("Input file missing: {}", input);
             std::process::exit(1);
-        },
-    };
-    solvers[&key](&input);
+        });
+    let solver = &solve::solvers()[&path];
+    solver(&input);
 }
