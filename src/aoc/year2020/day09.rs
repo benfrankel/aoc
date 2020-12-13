cefb8c64360929ce978997b20e3da6c6bc8cebe8
Ben Frankel <ben.frankel7@gmail.com>
2020-12-12 22:10:55 -0800

    Optimize day 9 part 1

--- src/aoc/year2020/day09.rs
+++ src/aoc/year2020/day09.rs
@@ -10,12 +10,16 @@ pub fn parse(input: &str) -> Vec<i64> {
 pub fn part1(a: &[i64]) -> i64 {
     let preamble = 25;
 
+    let mut window = a[0..preamble].to_vec();
+    window.sort_unstable();
     for i in preamble..a.len() {
-        let mut window = a[i - preamble..i].to_vec();
-        window.sort_unstable();
         if find_sum2(&window, a[i]).is_none() {
             return a[i];
         }
+        let idx = window.binary_search(&a[i - preamble]).unwrap_or_else(|x| x);
+        window.remove(idx);
+        let idx = window.binary_search(&a[i]).unwrap_or_else(|x| x);
+        window.insert(idx, a[i]);
     }
     
     panic!("Couldn't find an appropriate window.")
