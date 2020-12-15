4ce1977580ad35f2ef0a59f9542992d87887771a
Ben Frankel <ben.frankel7@gmail.com>
2020-12-14 21:31:52 -0800

    Day 15

---
+++ src/aoc/year2020/day15.rs
@@ -0,0 +1,45 @@
+use crate::prelude::*;
+
+pub fn parse(input: &str) -> Vec<usize> {
+    input
+        .trim()
+        .split(",")
+        .map(|x| x.parse().unwrap())
+        .collect()
+}
+
+pub fn part1(a: &[usize]) -> i64 {
+    let nth = 2020;
+
+    let mut prev = vec![None; nth];
+    for i in 0..a.len() - 1 {
+        prev[a[i]] = Some(i);
+    }
+
+    let mut num = a[a.len() - 1];
+    for i in a.len()..nth {
+        let age = (i - 1) - prev[num].unwrap_or(i - 1);
+        prev[num] = Some(i - 1);
+        num = age;
+    }
+
+    num as _
+}
+
+pub fn part2(a: &[usize]) -> i64 {
+    let nth = 30000000;
+
+    let mut prev = vec![None; nth];
+    for i in 0..a.len() - 1 {
+        prev[a[i]] = Some(i);
+    }
+
+    let mut num = a[a.len() - 1];
+    for i in a.len()..nth {
+        let age = (i - 1) - prev[num].unwrap_or(i - 1);
+        prev[num] = Some(i - 1);
+        num = age;
+    }
+
+    num as _
+}
